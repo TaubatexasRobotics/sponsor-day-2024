5f0f8d3c7b056af7b54f58f5e57990fdb4f57db0
Clara Arandiga <claraarandiga@gmail.com>
2024-08-17 11:00:39 -0300

    Robot Code

--- pieceSys.py
+++ pieceSys.py
@@ -8,21 +8,23 @@ class ShooterSys():
         self.controle = wpilib.Joystick(0)
 
         self.shooter = phoenix5.WPI_VictorSPX(5)
-        self.intake = rev.CANSparkMax(54, rev.CANSparkMax.MotorType.kBrushless)
+        self.intake = rev.CANSparkMax(9, rev.CANSparkMax.MotorType.kBrushless)
         self.threadmill = phoenix5.WPI_VictorSPX(1)
-        ()
 
     def shooterPeriodic(self):
-        self.intake_value = self.controle.getRawAxis(5) # Moves the intake
-        self.intake.set(self.intake_value)
+        if self.controle.getRawButton(4): # Moves the intake
+            self.intake.set(0.5)
+        elif self.controle.getRawButton(2):
+            self.intake.set(-0.2)
+        else:
+            self.intake.set(0)
 
-        if self.controle.getRawButton(2) == True:
-            self.threadmill.set(1.0)
+        if self.controle.getRawButton(3) == True: # Moves the threadmill
+            self.threadmill.set(-1.0)
         else:
             self.threadmill.set(0)
-        
-        if self.controle.getRawButton(3) == True:
-            self.shooter.set(1.0)
+
+        if self.controle.getRawButton(1) == True: # Fires the shooter
+            self.shooter.set(0.5)
         else:
             self.shooter.set(0)
-
